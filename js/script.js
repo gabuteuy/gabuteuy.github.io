2a20faba528835c1473497063d061344c3dda5a8
gabuteuy <mendozadante05@gmail.com>
2022-05-29 14:18:29 +0700

    chore: :weary: Emang Lagi Gabut

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
+function randImg(){
+    setTimeout(function () {
+        var rand = Math.floor(Math.random() * 6) + 1;
+        var img = document.querySelector("#qr");
+        img.src = "images/"+rand+".png";
 
+        randImg();
+    }, 15000);
+}
 
 function fn() {
-    setTimeout(function () {
-        var hint = document.querySelector("#hint");
-        hint.innerHTML += "Coba Decode Dulu";
-    }, 30000);
+    var img = document.querySelector("#qr");
+    img.src = "images/1.png";
+    randImg();
 }
 
 document.addEventListener('DOMContentLoaded', fn, false);
